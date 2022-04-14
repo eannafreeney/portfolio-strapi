2fc5489fa1ca488a04e58e41b13620a58174fb9a
Eanna <eannafreeney@gmail.com>
2022-04-14 14:09:47 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4b760dd37be2337ebdaadea6e737058'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '48621f69-49c8-49c0-8d16-f7e5d159c2ff'
+};
